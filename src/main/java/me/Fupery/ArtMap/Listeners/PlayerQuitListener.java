fe09038c50dc46cdd55d9fa28db86e1aecab19a3
wispoffates <fayted@gmail.com>
2018-07-23 00:57:55 -0400

    Fix players dieing with artkit causing inventory issues
    - On damage kick the player off the eisel
    - OnDeath now only handles preview so inventory drops shoudl work as
    expected.

--- src/main/java/me/Fupery/ArtMap/Listeners/PlayerQuitListener.java
+++ src/main/java/me/Fupery/ArtMap/Listeners/PlayerQuitListener.java
@@ -1,14 +1,16 @@
 package me.Fupery.ArtMap.Listeners;
 
-import me.Fupery.ArtMap.ArtMap;
 import org.bukkit.Material;
 import org.bukkit.entity.EntityType;
 import org.bukkit.entity.Player;
 import org.bukkit.event.EventHandler;
+import org.bukkit.event.entity.EntityDamageEvent;
 import org.bukkit.event.entity.PlayerDeathEvent;
 import org.bukkit.event.player.PlayerKickEvent;
 import org.bukkit.event.player.PlayerQuitEvent;
 import org.bukkit.event.player.PlayerTeleportEvent;
+
+import me.Fupery.ArtMap.ArtMap;
 
 class PlayerQuitListener implements RegisteredListener {
 
@@ -42,27 +44,48 @@ class PlayerQuitListener implements RegisteredListener {
         }
     }
 
-    @EventHandler
+	@EventHandler
     public void onPlayerDeath(PlayerDeathEvent event) {
         if (event.getEntity().getType() != EntityType.PLAYER) {
             return;
         }
         Player player = event.getEntity();
         if (ArtMap.getArtistHandler().containsPlayer(player)) {
-            event.setKeepInventory(true);
-            ArtMap.getArtistHandler().getCurrentSession(player).removeKit(player);
             ArtMap.getArtistHandler().removePlayer(player);
         }
         ArtMap.getPreviewManager().endPreview(player);
     }
 
+	@EventHandler
+	public void onPlayerDamage(EntityDamageEvent event) {
+		if (event.getEntity().getType() != EntityType.PLAYER) {
+			return;
+		}
+		Player player = Player.class.cast(event.getEntity());
+		if (ArtMap.getArtistHandler().containsPlayer(player)) {
+			ArtMap.getArtistHandler().getCurrentSession(player).removeKit(player);
+			ArtMap.getArtistHandler().removePlayer(player);
+		}
+		if (ArtMap.getPreviewManager().isPreviewing(player)) {
+			if (player.getItemInHand().getType() == Material.MAP) {
+				ArtMap.getPreviewManager().endPreview(player);
+			}
+		}
+	}
+
     @EventHandler
     public void onPlayerTeleport(final PlayerTeleportEvent event) {
         if (ArtMap.getArtistHandler().containsPlayer(event.getPlayer())) {
             if (event.getPlayer().isInsideVehicle()) {
+				ArtMap.getArtistHandler().getCurrentSession(event.getPlayer()).removeKit(event.getPlayer());
                 ArtMap.getArtistHandler().removePlayer(event.getPlayer());
             }
         }
+		if (ArtMap.getPreviewManager().isPreviewing(event.getPlayer())) {
+			if (event.getPlayer().getItemInHand().getType() == Material.MAP) {
+				ArtMap.getPreviewManager().endPreview(event.getPlayer());
+			}
+		}
     }
 
     @Override
