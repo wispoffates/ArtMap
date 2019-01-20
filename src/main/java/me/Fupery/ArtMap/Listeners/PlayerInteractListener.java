b5aaace74b192034be008900d0808512a817c3df
wispoffates <fayted@gmail.com>
2019-01-20 01:29:29 -0500

    - 3.0.5 - Actually fix the underground easel not allowed

--- src/main/java/me/Fupery/ArtMap/Listeners/PlayerInteractListener.java
+++ src/main/java/me/Fupery/ArtMap/Listeners/PlayerInteractListener.java
@@ -84,9 +84,9 @@ class PlayerInteractListener implements RegisteredListener {
         BlockFace facing = getFacing(player);
         Location frameBlock = new LocationHelper(easelLocation).shiftTowards(facing);
 
-        if (easelLocation.getBlock().isEmpty()
-                || baseLocation.getBlock().isEmpty()
-                || frameBlock.getBlock().isEmpty()
+        if (!easelLocation.getBlock().isEmpty()
+                || !baseLocation.getBlock().isEmpty()
+                || !frameBlock.getBlock().isEmpty()
                 || Easel.checkForEasel(easelLocation)) {
             Lang.ActionBar.INVALID_POS.send(player);
             EaselEffect.USE_DENIED.playEffect(baseLocation);
