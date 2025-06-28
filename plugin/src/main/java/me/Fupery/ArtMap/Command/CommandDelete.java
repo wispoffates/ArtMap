9a83184f44938bc25d05bbfabc4d614d8061aaad
wispoffates <fayted@gmail.com>
2025-06-28 16:58:41 -0400

    fix: fix /art delete error on no title supplied

--- plugin/src/main/java/me/Fupery/ArtMap/Command/CommandDelete.java
+++ plugin/src/main/java/me/Fupery/ArtMap/Command/CommandDelete.java
@@ -22,6 +22,10 @@ class CommandDelete extends AsyncCommand {
 			sender.sendMessage("Please use the Paint Brush to access the artwork for delete.");
 			return;
 		}
+        if (args.length<2) {
+           msg.message = String.format(Lang.MISSING_TITLE.get(), args[1]);
+            return;
+        }
         try {
             Optional<MapArt> art = ArtMap.instance().getArtDatabase().getArtwork(args[1]);
 
